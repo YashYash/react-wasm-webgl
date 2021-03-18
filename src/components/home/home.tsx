33c470824081c58561f9d65e76c7cb87ef067b18
YashYash <yash.saxena1217@gmail.com>
2021-03-17 22:08:03 -0700

    setup react router

---
+++ src/components/home/home.tsx
@@ -0,0 +1,10 @@
+import { HomeProps } from './home.types'
+import React from 'react'
+
+export const Home: React.FC<HomeProps> = props => {
+  return (
+    <div>
+      <p>Home here</p>
+    </div>
+  )
+}
