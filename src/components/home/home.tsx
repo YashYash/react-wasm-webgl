4bc63e8f17d193fbd2ac7a27b86b6a48acc94824
YashYash <yash.saxena1217@gmail.com>
2021-03-20 12:01:42 -0700

    push up latest

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,6 +2,10 @@ import { HomeProps } from './home.types'
 import React from 'react'
 
 export const Home: React.FC<HomeProps> = props => {
+  import('webgl-wasm-dev').then(module => {
+    const result = module.fib(1)
+    console.log(result)
+  })
   return (
     <div>
       <p>Home here</p>
